a363ae0be44d40f33b8ef1c7d7eb3fddceb93343
manoj-kumar22 <manoj-kumar22@hpe.com>
2016-06-13 15:36:33 +0530

    added lists

---
+++ dict.py
@@ -0,0 +1,10 @@
+urls = {'Google': 'http;//google.com',
+        'Pluralsight': "http://pluralsight.com",
+        "Sixty North": 'http://sixty-north.com',
+        "Microsoft": "http://microsoft.com"
+        }
+print(urls)
+urls['Google'] = 'http://facebook.com'
+nameAndAges = [('alice', 32), ('bob', 10)]
+d = dict(nameAndAges)
+print(d)
