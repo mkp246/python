7ab775b32ef1bcce8efb91a1f0ed2efdd147ff19
manoj-kumar22 <manoj-kumar22@hpe.com>
2016-06-13 16:27:31 +0530

    dictionary

--- dict.py
+++ dict.py
@@ -8,3 +8,14 @@ urls['Google'] = 'http://facebook.com'
 nameAndAges = [('alice', 32), ('bob', 10)]
 d = dict(nameAndAges)
 print(d)
+# copy used as .copy or constructor
+g = dict(wheat = 0x1232654, khaki = 0x869732, crimson = 0xde21ac)
+print(g)
+g.update(d)
+print(g)
+
+stocks = dict(GOOG=891,APPl=428,IBM=432)
+stocks.update({'GOOG':'654'})
+print(stocks)
+for key in stocks:
+    print("{key} => {value}".format(key=key, value=stocks[key]) )
